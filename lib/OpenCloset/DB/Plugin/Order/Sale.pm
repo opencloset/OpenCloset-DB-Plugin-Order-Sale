29a38179f70b2f34d4389cfbda1f15d8a1a365e8
Keedi Kim <keedi.k@gmail.com>
2017-02-17 12:06:41 +0900

    dzil new --profile foobar OpenCloset-DB-Plugin-Order-Sale

---
+++ lib/OpenCloset/DB/Plugin/Order/Sale.pm
@@ -0,0 +1,33 @@
+use utf8;
+
+package OpenCloset::DB::Plugin::Order::Sale;
+
+# ABSTRACT: OpenCloset::DB::Plugin::Order::Sale
+
+use utf8;
+use strict;
+use warnings;
+
+our $VERSION = '0.000';
+
+1;
+__END__
+
+=head1 SYNOPSIS
+
+    use OpenCloset::DB::Plugin::Order::Sale;
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+
+=head1 INSTALLATION
+
+L<OpenCloset::DB::Plugin::Order::Sale> uses well-tested and widely-used CPAN modules, so installation should be as simple as
+
+    $ cpanm --mirror=https://cpan.theopencloset.net --mirror=http://cpan.silex.kr --mirror-only OpenCloset::DB::Plugin::Order::Sale
+
+when using L<App::cpanminus>. Of course you can use your favorite CPAN client or install manually by cloning the L</"Source Code">.
